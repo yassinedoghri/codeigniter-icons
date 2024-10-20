29253fb225be563141a6155f0824d8e996e036b4
Yassine Doghri <yassine@doghri.fr>
2024-10-20 15:36:40 +0000

    refactor(helper): remove phpicon function and return PHPIcons object with icon function

--- src/Helpers/icons_helper.php
+++ src/Helpers/icons_helper.php
@@ -4,33 +4,18 @@ declare(strict_types=1);
 
 use PHPIcons\PHPIcons;
 
-if (! function_exists('phpicons')) {
-    /**
-     * Provides convenient access to the PHPIcons class
-     * for rendering icons
-     */
-    function phpicons(string $iconKey): PHPIcons
-    {
-        /** @var PHPIcons $phpicons */
-        $phpicons = service('phpicons');
-
-        return $phpicons
-            ->icon($iconKey);
-    }
-}
-
 if (! function_exists('icon')) {
     /**
      * Provides convenient access to rendering an icon with optional attributes using the PHPIcons class
      *
      * @param array<string, string> $attributes
      */
-    function icon(string $iconKey, array $attributes = []): string
+    function icon(string $iconKey, array $attributes = []): PHPIcons
     {
         /** @var PHPIcons $phpicons */
         $phpicons = service('phpicons');
 
-        return (string) $phpicons
+        return $phpicons
             ->icon($iconKey)
             ->attributes($attributes);
     }
